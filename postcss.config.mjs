1ba02982a60b02f95bdbe8239d464b52b4087be0
Vladislav <132088831+Lem0n4eLL0@users.noreply.github.com>
2026-06-19 17:02:10 +0400

    Настройка postcss и storybook

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,20 @@
 const config = {
   plugins: {
+    autoprefixer: {},
+
+    ...(process.env.NODE_ENV === 'production' && {
+      cssnano: {
+        preset: [
+          'default',
+          {
+            discardComments: { removeAll: true },
+            normalizeWhitespace: true,
+            minifyFontValues: true,
+            colormin: true,
+          },
+        ],
+      },
+    }),
   },
 };
 
