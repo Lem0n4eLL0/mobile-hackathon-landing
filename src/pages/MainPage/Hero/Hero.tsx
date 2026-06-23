25e2b35551001379fb828563888803c1cabd6426
Vladislav <132088831+Lem0n4eLL0@users.noreply.github.com>
2026-06-23 16:55:53 +0400

    Реализация компонента Hero без адаптива

---
+++ src/pages/MainPage/Hero/Hero.tsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx'
+import styles from './Hero.module.css'
+import commonStyles from '@/styles/common.module.css'
+
+export const Hero = () => {
+  return (
+    <div className={styles['hero__wrapper']}>
+      <div className={styles['hero']}>
+        <ul className={styles['hero__list']}>
+          <li className={styles['hero__list-item']}>23&nbsp;августа</li>
+        </ul>
+        <div className={styles['hero__content']}>
+          <h1 className={styles['hero__title']}>
+            Yandex
+            <br />
+            Mobile Runtime
+          </h1>
+          <p className={styles['hero__subtitle']}>
+            Фестиваль практики и&nbsp;технобатлов для&nbsp;мобильных разработчиков
+          </p>
+        </div>
+        <button className={clsx(commonStyles['pink_btn'], styles['hero__btn'])}>
+          Регистрация на&nbsp;фестивать
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,12 +2,15 @@ import { MainHeader } from '@/layouts/headers/MainHeader'
 import styles from './MainPage.module.css'
 import { BASE_HEADER_MENU_LINKS } from '@/common/common'
 import { MainFooter } from '@/layouts/footers/MainFooter'
+import { Hero } from './Hero'
 
 export const MainPage = () => {
   return (
-    <div className={styles.content__wrapper}>
+    <div className={styles['content__wrapper']}>
       <MainHeader menuLinks={BASE_HEADER_MENU_LINKS} />
-      <main className={styles.content}>Main Page</main>
+      <main className={styles['content']}>
+        <Hero />
+      </main>
       <MainFooter />
     </div>
   )
