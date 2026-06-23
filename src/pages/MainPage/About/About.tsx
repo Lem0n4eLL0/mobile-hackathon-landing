f3c74b9a642736e6affaa85c2fe2adcd7b386010
Vladislav <132088831+Lem0n4eLL0@users.noreply.github.com>
2026-06-23 17:59:16 +0400

    Реализован блок About

---
+++ src/pages/MainPage/About/About.tsx
@@ -0,0 +1,23 @@
+import styles from './About.module.css'
+
+export const About = () => {
+  return (
+    <div className={styles['about__wrapper']}>
+      <div className={styles['about']}>
+        <p className={styles['about__description']}>
+          Yandex Mobile Runtime&nbsp;&mdash; летний фестиваль практики и&nbsp;карьерного роста
+          для&nbsp;мобильных разработчиков. Встречаемся mobile-сообществом, чтобы обменяться опытом
+          и&nbsp;попробовать&nbsp;AI в&nbsp;реальных задачах.
+        </p>
+        <ul className={styles['about__list']}>
+          <li className={styles['about__list-item']}>Воркшопы</li>
+          <li className={styles['about__list-item']}>Карьерные консультации</li>
+          <li className={styles['about__list-item']}>AI-хакатон</li>
+          <li className={styles['about__list-item']}>Дискуссии</li>
+          <li className={styles['about__list-item']}>Квиз</li>
+          <li className={styles['about__list-item']}>Летний двор</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import styles from './MainPage.module.css'
 import { BASE_HEADER_MENU_LINKS } from '@/common/common'
 import { MainFooter } from '@/layouts/footers/MainFooter'
 import { Hero } from './Hero'
+import { About } from './About'
 
 export const MainPage = () => {
   return (
@@ -10,6 +11,7 @@ export const MainPage = () => {
       <MainHeader menuLinks={BASE_HEADER_MENU_LINKS} />
       <main className={styles['content']}>
         <Hero />
+        <About />
       </main>
       <MainFooter />
     </div>
